b22c39be652f8852edaa2fe59c48950bf7616361
Selorm Dake <123432641+seloDake@users.noreply.github.com>
2025-03-26 03:37:36 -0400

    Setr up the back bone for uni section. Will flesh out later.

--- AI_Project2_Resolution/lab2.py
+++ AI_Project2_Resolution/lab2.py
@@ -8,10 +8,10 @@ no order...
 
 Resolver...(PL-Resolution in R&N 7.5 on pg 228 for ideas)
 Unifictaion...(Unify in R&N 9.2 on pg 285 for ideas)
-function to parse through data as either(Clauses, Predicates, Terms)
+^function to parse through data as either(Clauses, Predicates, Terms)
     -Three helper functions to hanle each of these specific types
     -sort out terms as well 
-function to manage clauses
+^function to manage clauses
     - needs to store and check if clause has been seen before
     - also needs to store and compare said functions + create them?
 Unification function
@@ -27,6 +27,11 @@ sys entry: python3 AI_Project2_Resolution\lab2.py AI_Project2_Resolution/truck.c
 output: yes | no
 
 """
+
+"""
+Resolvers
+"""
+
 
 def cnf_parser(f_name):
     # This function will read and parse the cnf file
@@ -56,11 +61,11 @@ def cnf_parser(f_name):
                 section = "clauses"
             elif section == "clauses":
                 clauses.append(cl_parser(line))
-    #print(preds)
-    #print(variables)
-    #print(consts)
-    #print(functs)
-    print(clauses)
+    # print(preds)
+    # print(variables)
+    # print(consts)
+    # print(functs)
+    # print(clauses)
     return preds, variables, consts, functs, clauses
 
 
@@ -81,6 +86,35 @@ def pred_parser(lit):
     name, args = lit.split("(")
     args = args[:-1].split(",")  # delete ')' & ','
     return name, args
+
+"""
+Unify codes
+"""
+def unify(term1, term2, subs):
+    """
+    Whats needed??
+    -check if var?
+    -check if seen alr?
+    -combine vars
+    
+    begin to process the whole term with subsitution
+    if sub works return the sub, else NOne
+    """
+    ...
+
+def univar(var, term, subs):
+    # unify the vars if necessary. Return updated var, if any
+    ...
+
+def seen(var, term, subs):
+    # check if seen alr to avoid loops. Return Bool
+    ...
+
+def var_check(term):
+    # is it a var? Return bool
+    ...
+
+
 
 def main():
     # used to test and finally run this code.
