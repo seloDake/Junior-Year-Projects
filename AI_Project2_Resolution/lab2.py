ae5399ee55a792b91544a836a21de532ec5e4ec1
Selorm Dake <123432641+seloDake@users.noreply.github.com>
2025-03-26 03:00:21 -0400

    renamed main folder and created cnf parser. Tested it for all functions apart from clauses.

---
+++ AI_Project2_Resolution/lab2.py
@@ -0,0 +1,80 @@
+""" This will be my main program file for AI lab 2. """
+import sys
+import itertools
+""" 
+Gameplan for program
+--------------------
+no order...
+
+Resolver...(PL-Resolution in R&N 7.5 on pg 228 for ideas)
+Unifictaion...(Unify in R&N 9.2 on pg 285 for ideas)
+function to parse through data as either(Clauses, Predicates, Terms)
+    -Three helper functions to hanle each of these specific types
+    -sort out terms as well 
+function to manage clauses
+    - needs to store and check if clause has been seen before
+    - also needs to store and compare said functions + create them?
+Unification function
+    -What must be compared?
+    -How do you handle substituting a free variable for one of following?
+    -Another free variable
+    -A constant
+    -A function
+    -How do you create a new free variable to prevent name conflicts?
+    -How will you apply the substitutions to predicates and functions?
+
+sys entry: python3 lab2.py testcases/functions/f1.cnf
+output: yes | no
+
+"""
+
+def cnf_parser(f_name):
+    # This function will read and parse the cnf file
+    preds = set()
+    variables = set()
+    consts = set()
+    functs = set()
+    clauses = []
+
+    with open(f_name, 'r') as file:
+        section = None
+        for line in file:
+            line = line.strip()
+            # Skip empty lines and comments(if statements or switch?)
+            if not line or line.startswith("#"): 
+                continue
+            # put in categories
+            if line.startswith("Predicates:"):
+                preds.update(line.split(":")[1].split())
+            elif line.startswith("Variables:"):
+                variables.update(line.split(":")[1].split())
+            elif line.startswith("Constants:"):
+                consts.update(line.split(":")[1].split())
+            elif line.startswith("Functions:"):
+                functs.update(line.split(":")[1].split())
+            elif line.startswith("Clauses:"):
+                section = "clauses"
+            #elif section == "clauses":
+                #clauses.append(cl_parser(line))
+    print(preds)
+    print(variables)
+    print(consts)
+    print(functs)
+    return preds, variables, consts, functs, clauses
+
+
+def cl_parser(line):
+    # Will parse the individual lines in order tomake clauses
+    ...
+
+def pred_parser(lit):
+    # Seperates the pred name and its args(returns both)
+    ...
+
+def main():
+    # used to test and finally run this code.
+    cnf_parser(sys.argv[0])    
+
+
+if __name__ == "__main__":
+    main()
