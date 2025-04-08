f335a9f453368dd9d85a1da588ed954e7d6c5fbc
Selorm Dake <123432641+seloDake@users.noreply.github.com>
2025-04-07 22:55:18 -0400

    updated code

--- AI_Homework3/hw3.py
+++ AI_Homework3/hw3.py
@@ -1,1 +1,19 @@
-"""Code for solving hw3"""
+""" Code for solving hw3 """
+import sys
+import math
+from collections import Counter, defaultdict
+
+""" Read the data file and parse the info """
+def reader(d_file):
+   data = []
+   with open(d_file, 'r') as f:
+        for line in f:
+            sects = line.strip().split()
+            attributes = sects[:-1]
+            label = sects[-1]
+            #update the data for the code as booleans
+            data.append((list(map(lambda x: x == "True", attributes)), label))
+   return data
+
+def main():
+    ...
